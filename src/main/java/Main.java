2048e68146d788758d1c4d5bee3693956fd80a46
Godrik <krisapios@gmail.com>
2022-11-02 18:29:30 +0600

    load1

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints intsCalc = new IntsCalculator();
+        System.out.println(intsCalc.sum(2, 2));
+        System.out.println(intsCalc.sum(10, 22));
+        System.out.println(intsCalc.pow(2, 10));
+        System.out.println(intsCalc.div(100, 20));
+        System.out.println(intsCalc.mult(10,10));
+
+        Calculator calc = new Calculator();
+        System.out.println(
+                calc.newFormula()
+                        .addOperand(5)
+                        .addOperand(15)
+                        .calculate(Calculator.Operation.MULT)
+                        .result()
+        );
+    }
+}
